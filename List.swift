69e386b7bfa218f52a474b9e8009b8305dcd7c16
koher <koher@koherent.org>
2014-06-13 02:40:32 +0900

    Implemented the List class

---
+++ List.swift
@@ -0,0 +1,130 @@
+class List<T> : Sequence {
+	var elements: Array<T>
+	
+	init(_ elements: Array<T>) {
+		self.elements = elements
+	}
+	
+	convenience init() {
+		self.init([])
+	}
+	
+	convenience init(_ list: List<T>) {
+		self.init(list)
+	}
+	
+	convenience init(_ elements: T...) {
+		self.init(elements)
+	}
+	
+	subscript(index: Int) -> T {
+		get {
+			return elements[index]
+		}
+		set {
+			elements[index] = newValue
+		}
+	}
+	
+	subscript(subRange: Range<Int>) -> Slice<T> {
+		get {
+			return elements[subRange]
+		}
+		set {
+			elements[subRange] = newValue
+		}
+	}
+	
+	func append(newElement: T) {
+		elements.append(newElement)
+	}
+	
+	func insert(newElement: T, atIndex index: Int) {
+		elements.insert(newElement, atIndex: index)
+	}
+	
+	func removeAtIndex(index: Int) -> T {
+		return elements.removeAtIndex(index)
+	}
+	
+	func removeLast() -> T {
+		return elements.removeLast()
+	}
+	
+	func removeAll(keepCapacity: Bool = false) {
+		elements.removeAll(keepCapacity: keepCapacity)
+	}
+
+	func reserveCapacity(minimumCapacity: Int) {
+		elements.reserveCapacity(minimumCapacity)
+	}
+
+	var count: Int {
+		get {
+			return elements.count
+		}
+	}
+	
+	var isEmpty: Bool {
+		get {
+			return elements.isEmpty
+		}
+	}
+
+	var capacity: Int {
+		get {
+			return elements.capacity
+		}
+	}
+	
+	func sort(isOrderedBefore: (T, T) -> Bool) {
+		elements.sort(isOrderedBefore)
+	}
+	
+	func reverse() -> Array<T> {
+		return elements.reverse()
+	}
+
+	func filter(includeElement: (T) -> Bool) -> List<T> {
+		return List(elements.filter(includeElement))
+	}
+	
+	func map<U>(transform: (T) -> U) -> List<U> {
+		return List<U>(elements.map(transform))
+	}
+
+	func reduce<U>(initial: U, combine: (U, T) -> U) -> U {
+		return elements.reduce(initial, combine)
+	}
+	
+	func generate() -> ListGenerator<T> {
+		return ListGenerator(self)
+	}
+}
+
+@assignment func += <T>(inout lhs: List<T>, rhs: T) {
+	lhs.elements += rhs
+}
+
+@assignment func += <T>(inout lhs: List<T>, rhs: List<T>) {
+	lhs.elements += rhs.elements
+}
+
+@assignment func += <T>(inout lhs: List<T>, rhs: Array<T>) {
+	lhs.elements += rhs
+}
+
+struct ListGenerator<T> : Generator {
+	let list : List<T>
+	var index : Int
+	
+	init(_ list: List<T>) {
+		self.list = list
+		index = 0
+	}
+	
+	mutating func next() -> T? {
+		if index >= list.count { return nil }
+		return list[index++]
+	}
+}
--- README.md
+++ README.md
@@ -1,4 +1,57 @@
 Swift-List
 ==========
 
-The List class wrapping Array for Swift
+The List class wraps Swift's Array to use it as a reference type.
+
+What for?
+---------
+
+The behavior of Swift's Array is sometimes confusing. The code below is an example.
+
+```swift
+var a = [11, 22, 33]
+
+var b = a
+a[0] = 777 // b[0] also becomes 777
+
+a.append(44)
+a[0] = 888 // b[0] does not become 888
+```
+
+It is because Swift's Array is a structure and so a value type. The List class wraps it and we can use it as a reference type.
+
+Usage
+-----
+
+You can use the List class like the Array.
+
+```swift
+var list = List(2, 3, 5, 7, 11, 13)
+
+list.append(17)
+list += 19
+
+for element in list {
+    println(element)
+}
+```
+
+What's the difference?
+----------------------
+
+Because the List class is a reference type, it does not cause the confusing behavior above.
+
+```swift
+var a = List(11, 22, 33)
+
+var b = a
+a[0] = 777
+
+a.append(44)
+a[0] = 888 // b[0] also becomes 888
+```
+
+License
+-------
+
+MIT License. See the LICENSE file.
